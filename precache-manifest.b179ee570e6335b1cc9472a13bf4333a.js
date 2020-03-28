3751f3a8504e67428a5c958f47bd431b7feebfb5
Muhamad Ismuaji <muhamadismuaji@gmail.com>
2020-03-29 00:43:39 +0700

    Updates

---
+++ precache-manifest.b179ee570e6335b1cc9472a13bf4333a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d136b7349e5e28c4ceedddc7e5c25633",
+    "url": "/covid19map/index.html"
+  },
+  {
+    "revision": "951bd712b0160b23130a",
+    "url": "/covid19map/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "4d404761234e21058139",
+    "url": "/covid19map/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "951bd712b0160b23130a",
+    "url": "/covid19map/static/js/2.63f74a6b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/covid19map/static/js/2.63f74a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d404761234e21058139",
+    "url": "/covid19map/static/js/main.036ce238.chunk.js"
+  },
+  {
+    "revision": "0952a3d6064cfe606198",
+    "url": "/covid19map/static/js/runtime-main.0d55e238.js"
+  }
+]);
